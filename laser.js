6812ee120efb97d5f710d3ada8a61e5e99e309fc
Sam Sims <samsimss98@gmail.com>
2016-12-23 13:54:08 +0000

    Missing semicolons

--- laser.js
+++ laser.js
@@ -1,40 +1,37 @@
-function laser(spos, angle){
+function laser(spos, angle) {
   this.pos = createVector(spos.x, spos.y);
   this.vel = p5.Vector.fromAngle(angle);
   this.vel.mult(8);
 
-  this.update = function(){
+  this.update = function() {
     this.pos.add(this.vel);
-  }
-  this.render = function(){
+  };
+  this.render = function() {
     push();
     stroke(255);
     strokeWeight(4);
     point(this.pos.x, this.pos.y);
     pop();
-  }
+  };
 
-  this.hits = function(asteroid){
-    var d = dist(this.pos.x,this.pos.y, asteroid.pos.x, asteroid.pos.y);
-    if(d < asteroid.r){
+  this.hits = function(asteroid) {
+    var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+    if (d < asteroid.r) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
-  }
+  };
 
-  this.offscreen = function(){
-    if(this.pos.x > width || this.pos.x < 0){
+  this.offscreen = function() {
+    if (this.pos.x > width || this.pos.x < 0) {
       return true;
-    }
-    else if(this.pos.y > height || this.pos.y < 0){
+    } else if (this.pos.y > height || this.pos.y < 0) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
-  }
+  };
 
 
 }
